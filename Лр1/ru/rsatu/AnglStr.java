6729e84eaef27b2de77ed8021e43539ea97f5239
Alexander <bae11@yandex.ru>
2022-09-08 11:10:50 +0300

    Лабораторная 1

---
+++ Лр1/ru/rsatu/AnglStr.java
@@ -0,0 +1,17 @@
+package ru.rsatu;
+
+import ru.rsatu.rus.RusStr;
+
+public class AnglStr{
+
+public static void main(String[] args){
+
+System.out.println("Hello");
+RusStr.Rus();
+
+RusStr rus=new RusStr();
+rus.Obect();
+
+}
+
+}
---
+++ Лр1/ru/rsatu/rus/RusStr.java
@@ -0,0 +1,17 @@
+package ru.rsatu.rus;
+
+public class RusStr{
+
+public static void Rus(){
+
+System.out.println("Привет");
+
+}
+
+public void Obect(){
+
+	System.out.println("Привет от объекта");
+
+}
+
+}
